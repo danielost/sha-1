4236b9d9cdc0d69f073447d063fb10f9ce3ad6a5
Daniel <danya.ostap@gmail.com>
2023-11-08 16:01:06 +0200

    Add comments to `rand.go` and extend the charset

--- rand.go
+++ rand.go
@@ -2,11 +2,15 @@ package sha1
 
 import "math/rand"
 
+// randSeq generates a random string of a specified length.
 func randSeq(n int) string {
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	// Define the character set to use for random string generation:
+	var charset = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~")
 	b := make([]rune, n)
+
+	// Generate random characters from the character set and append them to the buffer.
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = charset[rand.Intn(len(charset))]
 	}
 	return string(b)
 }
